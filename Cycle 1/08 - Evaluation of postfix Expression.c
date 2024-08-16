aa5fef874fb4a0d5040c8dcdfd504f54c94a2725
ramsptp <85539763+ramsptp@users.noreply.github.com>
2024-08-16 21:29:13 +0530

    Update 08 - Evaluation of postfix Expression.c
    updated

--- Cycle 1/08 - Evaluation of postfix Expression.c
+++ Cycle 1/08 - Evaluation of postfix Expression.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
-char stack[20];
+
+int stack[20];  
 int top = -1;
-void push(char x)
+
+void push(int x)
 {
     stack[++top] = x;
 }
-char pop()
+
+int pop()
 {
     if (top == -1)
         return -1;
     else
         return stack[top--];
 }
-int operation(int a, int b, int current_character)
+
+int operation(int a, int b, char current_character)
 {
     switch (current_character)
     {
@@ -25,23 +29,30 @@ int operation(int a, int b, int current_character)
         return a * b;
     case '/':
         return a / b;
+    default:
+        return 0;  
     }
 }
+
 void main()
 {
-    char exp[20], x;
-    int num;
+    char exp[20];
+    int num, a, b;
     printf("Enter the expression: ");
     scanf("%s", exp);
     for (int i = 0; exp[i] != '\0'; i++)
     {
         if (isalnum(exp[i]))
         {
-            num = exp[i] - 48;
+            num = exp[i] - '0';  //same as exp[i] - 48
             push(num);
         }
         else
-            push(operation(pop(), pop(), exp[i]));
+        {
+            b = pop();  
+            a = pop();  
+            push(operation(a, b, exp[i]));
+        }
     }
-    printf("The result of %s is %d", exp, pop());
+    printf("The result of %s is %d\n", exp, pop());
 }
